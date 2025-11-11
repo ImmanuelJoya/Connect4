df0b1d7e45d19b6514fc8b9664520f37872bb40a
Immanuel Joy <immanueljoy107@gmail.com>
2025-11-11 15:23:13 -0500

    added preview port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    //  Ensure the app is listening on all network interfaces
+    host: '0.0.0.0',
+    // Allow the Render domain to access the application
+    allowedHosts: [
+      'connect-4-cz3b.onrender.com', 
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
